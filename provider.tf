8f4448b2d502295ec9e7e9d26a808b2a2a1e2734
Sarthak Upreti <76445510+Sarthak1947@users.noreply.github.com>
2026-02-03 09:55:59 +0530

    Add files via upload
    This Terraform configuration retrieves an existing Azure Virtual Machine and creates a snapshot of its OS disk. It uses a service principal for authentication, dynamically references the VM’s managed OS disk, and provisions the snapshot in the specified resource group and region. The snapshot is tagged for workload identification, automation tracking, and production environment management, making it suitable for AVD backup, recovery, or image preparation scenarios.

---
+++ provider.tf
@@ -0,0 +1,25 @@
+terraform {
+    required_version = ">=1.40.0"
+
+    required_providers{
+    azurerm = {
+        name = "hashicorp/azurerm"
+        version = ">=4.0.0"
+    }
+
+}
+
+}
+
+provider "azurerm"{
+    features{}
+
+
+subscription_id = var.subscription_id
+tenant_id = var.tenant_id
+client_id = var.client_id
+
+client_secret = var.client_secret
+
+}
+
