b12c1397025bfc65641242dcf26b39357d89edf2
Sarthak Upreti <76445510+Sarthak1947@users.noreply.github.com>
2026-02-03 10:07:46 +0530

    Add files via upload
    This Terraform configuration creates an Azure Managed Disk from an existing snapshot. It authenticates using a service principal, references the source snapshot dynamically, and provisions a managed disk with the specified SKU, location, and resource group. The disk is tagged for production use, automation tracking, and AVD workload management, making it suitable for VM recovery, scaling, or image-based deployments.

--- Project-2-ManagedDiskCreation/main.tf
+++ Project-2-ManagedDiskCreation/main.tf
@@ -1,1 +1,26 @@
 
+data "snapshot_name" "source_snapshot"{
+    name = var.snapshot_name
+    location = var.location
+    resource_group_name = var.snapshot_rg
+}
+
+
+resource "azurerm_managed_disk" "managed_disk_from_snapshot" {
+    name = var.disk_name
+    location = var.location
+    resource_group_name = var.disk_rg
+
+    storage_account_type = var.disk_sku
+    create_option = copy
+    source_resource_id = data.azurerm_snapshot.source_snapshot_id
+
+ tags = {
+    Environment  = "Prod"
+    CreatedFrom  = "Snapshot"
+    CreatedBy    = "Terraform"
+    Workload     = "AVD"
+    Automation   = "True"
+  }
+
+}
---
+++ Project-2-ManagedDiskCreation/variable.tf
@@ -0,0 +1,41 @@
+variable "client_id"{
+    type = string
+}
+
+variable "subscription_id"{
+    type =string
+}
+
+variable "snapshotname"{
+    type = string
+}
+
+variable "client_secret"{
+    type = string
+}
+
+variable "snapshot_rg"{
+    type=string
+}
+
+variable "disk_name"{
+    type = string
+
+}
+
+variable "disk_rg"{
+    type = string
+}
+
+variable "location"{
+    type = string
+}
+
+variable "disk_sku" {
+    type = string
+}
+
+variable "tenant_id"{
+    type =string
+}
+
